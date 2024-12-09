d7494780f14e4f7e7ae186780c2e1d5efed5da99
kreepsteam <pelmennagets@gmail.com>
2024-12-09 11:49:57 +0300

    trt
    rtert

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtClient = new System.Windows.Forms.TextBox();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.txtProblemDescription = new System.Windows.Forms.TextBox();
+            this.txtFaultType = new System.Windows.Forms.TextBox();
+            this.txtEquipment = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAddRequest = new System.Windows.Forms.Button();
+            this.database1DataSet = new WindowsFormsApp3.Database1DataSet();
+            this.database1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtClient
+            // 
+            this.txtClient.Location = new System.Drawing.Point(482, 84);
+            this.txtClient.Name = "txtClient";
+            this.txtClient.Size = new System.Drawing.Size(150, 20);
+            this.txtClient.TabIndex = 3;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(638, 83);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(150, 21);
+            this.cmbStatus.TabIndex = 4;
+            // 
+            // txtProblemDescription
+            // 
+            this.txtProblemDescription.Location = new System.Drawing.Point(326, 83);
+            this.txtProblemDescription.Name = "txtProblemDescription";
+            this.txtProblemDescription.Size = new System.Drawing.Size(150, 20);
+            this.txtProblemDescription.TabIndex = 5;
+            // 
+            // txtFaultType
+            // 
+            this.txtFaultType.Location = new System.Drawing.Point(168, 84);
+            this.txtFaultType.Name = "txtFaultType";
+            this.txtFaultType.Size = new System.Drawing.Size(150, 20);
+            this.txtFaultType.TabIndex = 6;
+            // 
+            // txtEquipment
+            // 
+            this.txtEquipment.Location = new System.Drawing.Point(12, 83);
+            this.txtEquipment.Name = "txtEquipment";
+            this.txtEquipment.Size = new System.Drawing.Size(150, 20);
+            this.txtEquipment.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Оборудование";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Тип неисправности";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(354, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Описание проблемы";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(539, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Клиент";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(693, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Статус";
+            // 
+            // btnAddRequest
+            // 
+            this.btnAddRequest.Location = new System.Drawing.Point(680, 151);
+            this.btnAddRequest.Name = "btnAddRequest";
+            this.btnAddRequest.Size = new System.Drawing.Size(75, 23);
+            this.btnAddRequest.TabIndex = 13;
+            this.btnAddRequest.Text = "ДОБАВИТЬ";
+            this.btnAddRequest.UseVisualStyleBackColor = true;
+            this.btnAddRequest.Click += new System.EventHandler(this.btnAddRequest_Click);
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // database1DataSetBindingSource
+            // 
+            this.database1DataSetBindingSource.DataSource = this.database1DataSet;
+            this.database1DataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddRequest);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEquipment);
+            this.Controls.Add(this.txtFaultType);
+            this.Controls.Add(this.txtProblemDescription);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.txtClient);
+            this.Name = "Form1";
+            this.Text = "Оборудования";
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtClient;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.TextBox txtProblemDescription;
+        private System.Windows.Forms.TextBox txtFaultType;
+        private System.Windows.Forms.TextBox txtEquipment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAddRequest;
+        private System.Windows.Forms.BindingSource database1DataSetBindingSource;
+        private Database1DataSet database1DataSet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadStatuses();
+        }
+
+        private void LoadStatuses()
+        {
+            cmbStatus.Items.Add("В ожидании");
+            cmbStatus.Items.Add("В работе");
+            cmbStatus.Items.Add("Выполнено");
+            cmbStatus.SelectedIndex = 0; // Установка значения по умолчанию
+        }
+
+
+        private void btnAddRequest_Click(object sender, EventArgs e)
+        {
+            // Проверка введенных данных
+            if (string.IsNullOrEmpty(txtEquipment.Text) || string.IsNullOrEmpty(txtFaultType.Text) ||
+                string.IsNullOrEmpty(txtProblemDescription.Text) || string.IsNullOrEmpty(txtClient.Text))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля.");
+                return;
+            }
+
+            // Создаем строку подключения
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // SQL-запрос на вставку данных (без RequestID)
+                string query = "INSERT INTO RepairRequests (Equipment, FaultType, ProblemDescription, Client, Stat) " +
+                               "VALUES (@Equipment, @FaultType, @ProblemDescription, @Client, @Stat)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Equipment", txtEquipment.Text);
+                    command.Parameters.AddWithValue("@FaultType", txtFaultType.Text);
+                    command.Parameters.AddWithValue("@ProblemDescription", txtProblemDescription.Text);
+                    command.Parameters.AddWithValue("@Client", txtClient.Text);
+
+                    // Получение значения статуса
+                    string selectedStatus = cmbStatus.SelectedItem.ToString();
+
+                    // Проверка на допустимое значение статуса
+                    if (selectedStatus != "В ожидании" && selectedStatus != "В работе" && selectedStatus != "Выполнено")
+                    {
+                        MessageBox.Show("Недопустимое значение статуса.");
+                        return;
+                    }
+
+                    command.Parameters.AddWithValue("@Stat", selectedStatus);
+
+                    try
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Заявка успешно добавлена.");
+                    }
+                    catch (SqlException sqlEx)
+                    {
+                        MessageBox.Show("Ошибка при добавлении заявки: " + sqlEx.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Произошла непредвиденная ошибка: " + ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+    }
+}
